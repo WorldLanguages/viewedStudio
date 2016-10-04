213c4497ac0c855ce93d5b704ae2f191491d5c24
World_Languages <mundofinky@gmail.com>
2016-10-04 20:06:25 -0300

    Update code.user.js

--- code.user.js
+++ code.user.js
@@ -7,15 +7,11 @@
 // @grant        none
 // ==/UserScript==
 
-sid =
-
-3464286               // ←←←←←← Change that number to the ID of the studio you recently created
-
-;
-
-
-
-
+sid = readCookie('sid');
+console.log('Studio: ' + sid);
+if (sid == null) {
+sid = prompt('Please paste the studio ID');
+createCookie('sid',sid,1000);}
 
 
 
@@ -31,3 +27,26 @@ var id = id1.replace(/\D/g,'');
 console.log('Project id: '+id);
 
 $.ajax({type: "PUT",url: "https://scratch.mit.edu/site-api/projects/in/" + sid + "/add/?pks=" + id,data: "pks=" + id});
+
+
+
+function readCookie(name) {
+	var nameEQ = name + "=";
+	var ca = document.cookie.split(';');
+	for(var i=0;i < ca.length;i++) {
+		var c = ca[i];
+		while (c.charAt(0)==' ') c = c.substring(1,c.length);
+		if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+	}
+	return null;
+}
+
+function createCookie(name,value,days) {
+	if (days) {
+		var date = new Date();
+		date.setTime(date.getTime()+(days*24*60*60*1000));
+		var expires = "; expires="+date.toGMTString();
+	}
+	else var expires = "";
+	document.cookie = name+"="+value+expires+"; path=/";
+}
